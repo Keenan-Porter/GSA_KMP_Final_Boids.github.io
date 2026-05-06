607f3eb547b8fba3fff0ca5f38c951a014451cf4
Keenan-Porter <116773589+Keenan-Porter@users.noreply.github.com>
2026-05-06 09:36:56 -0400

    Sorry no, using many particles

--- compute.wgsl
+++ compute.wgsl
@@ -1,71 +1,23 @@
-@group(0) @binding(0) var<uniform> res: vec2f;
-@group(0) @binding(1) var<uniform> frame : f32;
-@group(0) @binding(2) var<uniform> time: f32;
-@group(0) @binding(3) var<uniform> aDisp: f32;
-@group(0) @binding(4) var<uniform> bDisp: f32;
-@group(0) @binding(5) var<uniform> feed: f32;
-@group(0) @binding(6) var<uniform> kill: f32;
-@group(0) @binding(7) var<uniform> leftDiff: f32;
-@group(0) @binding(8) var<uniform> rightDiff: f32;
-@group(0) @binding(9) var<uniform> topDiff: f32;
-@group(0) @binding(10) var<uniform> topleftDiff: f32;
-@group(0) @binding(11) var<uniform> toprightDiff: f32;
-@group(0) @binding(12) var<storage> statein: array<f32>;
-@group(0) @binding(13) var<storage, read_write> stateout: array<f32>;
+struct Particle {
+  pos: vec2f,
+  vel: vec2f
+};
 
-fn index( x:i32, y:i32 ) -> u32 {
-  let _res = vec2i(res);
-  return u32( (y % _res.y) * _res.x + ( x % _res.x ) ) * 2;
+@group(0) @binding(0) var<uniform> res:   vec2f;
+@group(0) @binding(1) var<storage, read_write> state: array<Particle>;
+
+fn cellindex( cell:vec3u ) -> u32 {
+  let size = 8u;
+  return cell.x + (cell.y * size) + (cell.z * size * size);
 }
 
 @compute
 @workgroup_size(8,8)
-fn cs( @builtin(global_invocation_id) _cell:vec3u ) {
-  let cell = vec3i(_cell);
 
-  let a = statein[index(cell.x, cell.y)];
-  let b = statein[index(cell.x, cell.y) + 1];
-
-  let da = aDisp;
-  let db = bDisp;
-
-  let feed = feed;
-  let kill = kill;
-  let delta_time = time;
-
-  let i = index(cell.x, cell.y);
-  var la = statein[i] * -1.0;
-  var lb = statein[i + 1] * -1.0;
-
-  let lw = leftDiff;
-  let rw = rightDiff;
-  let tw = topDiff;
-  let bw = topDiff;
-  let ltw = topleftDiff;
-  let rtw = toprightDiff;
-  let lbw = topleftDiff;
-  let rbw = toprightDiff;
-
-  la += statein[ index(cell.x + 1, cell.y + 1) ] * rbw +
-                        statein[ index(cell.x + 1, cell.y)     ] * rw +
-                        statein[ index(cell.x + 1, cell.y - 1) ] * rtw +
-                        statein[ index(cell.x, cell.y - 1)     ] * tw +
-                        statein[ index(cell.x - 1, cell.y - 1) ] * ltw +
-                        statein[ index(cell.x - 1, cell.y)     ] * lw +
-                        statein[ index(cell.x - 1, cell.y + 1) ] * lbw +
-                        statein[ index(cell.x, cell.y + 1)     ] * bw;
-  
-  lb += statein[ index(cell.x + 1, cell.y + 1) + 1 ] * rbw +
-                        statein[ index(cell.x + 1, cell.y) + 1     ] * rw +
-                        statein[ index(cell.x + 1, cell.y - 1) + 1 ] * rtw +
-                        statein[ index(cell.x, cell.y - 1) + 1     ] * tw +
-                        statein[ index(cell.x - 1, cell.y - 1) + 1 ] * ltw +
-                        statein[ index(cell.x - 1, cell.y) + 1     ] * lw +
-                        statein[ index(cell.x - 1, cell.y + 1) + 1 ] * lbw +
-                        statein[ index(cell.x, cell.y + 1) + 1     ] * bw;
-
-  let na = a + ((da * la) - (a * b * b) + (feed * (1.0 - a))) * delta_time;
-  let nb = b + ((db * lb) + (a * b * b) - ((kill + feed) * b)) * delta_time;
-  stateout[i] = na;
-  stateout[i + 1] = nb;
+fn cs(@builtin(global_invocation_id) cell:vec3u)  {
+  let i = cellindex( cell );
+  let p = state[ i ];
+  var next = p.pos + (2. / res) * p.vel;
+  if( next.x >= 1. ) { next.x -= 2.; }
+  state[i].pos = next;
 }
--- main.js
+++ main.js
@@ -1,195 +1,46 @@
-import { default as seagulls } from './gulls/gulls.js'
-import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.min.js';
+import { default as gulls } from '../../gulls.js'
 
-const sg      = await seagulls.init(),
-      frag    = await seagulls.import( './frag.wgsl' ),
-      compute = await seagulls.import( './compute.wgsl' ),
-      render  = seagulls.constants.vertex + frag,
-      size    = (window.innerWidth * window.innerHeight),
-      state   = new Float32Array( size * 2 )
+const sg = await gulls.init(),
+      render_shader  = await gulls.import( './frag.wgsl' ),
+      compute_shader = await gulls.import( './compute.wgsl' )
 
-const pane = new Pane()
-const time = {
-  view: 'slider',
-  label: 'time',
-  min: 0.0,
-  max: 1.0,
-  value: 1.0
-}
-const timeBlade = pane.addBlade(time, 'time')
-let timebuffer = sg.uniform( time.value )
-timeBlade.on('change', (ev) => {
-  time.value = ev.value
-  timebuffer.value = ev.value
-})
-const adisp = {
-  view: 'slider',
-  label: 'A Dispersal',
-  min: 0.0,
-  max: 1.0,
-  value: 1.0
-}
-const adispBlade = pane.addBlade(adisp, 'A Dispersal')
-let adispbuffer = sg.uniform( adisp.value )
-adispBlade.on('change', (ev) => {
-  adisp.value = ev.value
-  adispbuffer.value = ev.value
-})
-const bdisp = {
-  view: 'slider',
-  label: 'B Dispersal',
-  min: 0.0,
-  max: 1.0,
-  value: 0.5
+const NUM_PARTICLES = 1024, 
+      NUM_PROPERTIES = 4, 
+      state = new Float32Array( NUM_PARTICLES * NUM_PROPERTIES )
+
+for( let i = 0; i < NUM_PARTICLES * NUM_PROPERTIES; i+= NUM_PROPERTIES ) {
+  state[ i ] = -1 + Math.random() * 2
+  state[ i + 1 ] = -1 + Math.random() * 2
+  state[ i + 2 ] = Math.random() * 10
 }
 
-const bdispBlade = pane.addBlade(bdisp, 'B Dispersal')
-let bdispbuffer = sg.uniform( bdisp.value )
-bdispBlade.on('change', (ev) => {
-  bdisp.value = ev.value
-  bdispbuffer.value = ev.value
-})
-const feed = {
-  view: 'slider',
-  label: 'feed',
-  min: 0.01,
-  max: 0.1,
-  value: 0.055
-}
+const state_b = sg.buffer( state ),
+      frame_u = sg.uniform( 0 ),
+      res_u   = sg.uniform([ sg.width, sg.height ]) 
 
-const feedBlade = pane.addBlade(feed, 'Feed')
-let feedbuffer = sg.uniform( feed.value )
-feedBlade.on('change', (ev) => {
-  feed.value = ev.value
-  feedbuffer.value = ev.value
-})
-const kill = {
-  view: 'slider',
-  label: 'kill',
-  min: 0.045,
-  max: 0.07,
-  value: 0.062
-}
-const killBlade = pane.addBlade(kill, 'Kill')
-let killbuffer = sg.uniform( kill.value )
-killBlade.on('change', (ev) => {
-  kill.value = ev.value
-  killbuffer.value = ev.value
-})
-
-const leftDiff = {
-  view: 'slider',
-  label: 'left Diffusion',
-  min: 0.0,
-  max: 1.0,
-  value: 0.2
-}
-const leftDiffBlade = pane.addBlade(leftDiff, 'Left Diffusion')
-let leftDiffbuffer = sg.uniform( leftDiff.value )
-leftDiffBlade.on('change', (ev) => {
-  leftDiff.value = ev.value
-  leftDiffbuffer.value = ev.value
-}) 
-const rightDiff = {
-  view: 'slider',
-  label: 'right Diffusion',
-  min: 0.0,
-  max: 1.0,
-  value: 0.2
-}
-const rightDiffBlade = pane.addBlade(rightDiff, 'Right Diffusion')
-let rightDiffbuffer = sg.uniform( rightDiff.value )
-rightDiffBlade.on('change', (ev) => {
-  rightDiff.value = ev.value
-  rightDiffbuffer.value = ev.value
-})
-const topDiff = {
-  view: 'slider',
-  label: 'top Diffusion',
-  min: 0.0,
-  max: 1.0,
-  value: 0.2
-}
-const topDiffBlade = pane.addBlade(topDiff, 'Top Diffusion')
-let topDiffbuffer = sg.uniform( topDiff.value )
-topDiffBlade.on('change', (ev) => {
-  topDiff.value = ev.value
-  topDiffbuffer.value = ev.value
-})
-const topleftDiff = {
-  view: 'slider',
-  label: 'top Left Diffusion',
-  min: 0.0,
-  max: 1.0,
-  value: 0.05
-}
-const topleftDiffBlade = pane.addBlade(topleftDiff, 'Top Left Diffusion')
-let topleftDiffbuffer = sg.uniform( topleftDiff.value )
-topleftDiffBlade.on('change', (ev) => {
-  topleftDiff.value = ev.value
-  topleftDiffbuffer.value = ev.value
-})
-const toprightDiff = {
-  view: 'slider',
-  label: 'top right Diffusion',
-  min: 0.0,
-  max: 1.0,
-  value: 0.05
-}
-const toprightDiffBlade = pane.addBlade(toprightDiff, 'Top Right Diffusion')
-let toprightDiffbuffer = sg.uniform( toprightDiff.value )
-toprightDiffBlade.on('change', (ev) => {
-  toprightDiff.value = ev.value
-  toprightDiffbuffer.value = ev.value
-})
-
-
-
-for( let i = 0; i < size; i++ ) {
-  state[ i * 2 ] = 1.0
-  state[(i * 2) + 1] = 0.0
-}
-
-for(let i = 475; i < 525; i++){
-  for(let j = -25; j < 25; j++){
-    state[(i * window.innerWidth + j) * 2 + 1] = 1.0
-  } 
-}
-
-const statebuffer1 = sg.buffer( state )
-const statebuffer2 = sg.buffer( state )
-const res = sg.uniform([ window.innerWidth, window.innerHeight ])
-
-let frame_u = sg.uniform(0)
-
-const renderPass = await sg.render({
-  shader: render,
+const render = await sg.render({
+  shader: render_shader,
   data: [
-    res,
-    sg.pingpong( statebuffer1, statebuffer2 )
-  ]
-})
-
-const computePass = sg.compute({
-  shader: compute,
-  data: [ 
-    res,
     frame_u,
-    timebuffer,
-    adispbuffer,
-    bdispbuffer,
-    feedbuffer,
-    killbuffer,
-    leftDiffbuffer,
-    rightDiffbuffer,
-    topDiffbuffer,
-    topleftDiffbuffer,
-    toprightDiffbuffer,
-    sg.pingpong( statebuffer1, statebuffer2 ) ],
-  onframe () { 
-  frame_u.value++
-  },
-  dispatchCount:  [Math.round(seagulls.width / 8), Math.round(seagulls.height/8), 1],
+    res_u,
+    state_b
+  ],
+  onframe() { frame_u.value++ },
+  count: NUM_PARTICLES,
+  blend: true
 })
 
-sg.run( computePass, renderPass )
+
+const dc = Math.ceil( NUM_PARTICLES / 64 )
+
+const compute = sg.compute({
+  shader: compute_shader,
+  data:[
+    res_u,
+    state_b
+  ],
+  dispatchCount: [ dc, dc, 1 ] 
+
+})
+
+sg.run( compute, render )
